37b6f8889ad6da682cdf98feece61b949501c124
MalBourbie <MalBourbie@gmail.com>
2025-01-22 20:26:48 +0100

    add footer content

--- src/layout/Footer/index.jsx
+++ src/layout/Footer/index.jsx
@@ -1,9 +1,20 @@
+import emailIcon from '../../assets/icons/email.svg'
+import githubIcon from '../../assets/icons/github.svg'
+import m from './index.module.scss'
+
 function Footer() {
     return (
-        <footer>
-            {/* Lien utile style github, linkedin... en mode icon */}
-            {/* mon nom + version ? */}
-            <p>by ...</p>
+        <footer className={m.footer}>
+            <div className={m.linkContainer}>
+                <a href="mailto:tomhoekstra27@gmail.com">
+                    <img src={emailIcon} alt="email of Tom Hoekstra" className={m.linkContainer__img}/>
+                </a>
+                <a href="https://github.com/LPHoekstra">
+                    <img src={githubIcon} alt="github of Tom Hoekstra" className={m.linkContainer__img}/>
+                </a>
+            </div>
+            <p>2025 Tom Hoekstra</p>
+            <span>version 0.1.0</span>
         </footer>
     )
 }
