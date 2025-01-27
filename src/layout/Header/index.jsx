e659e0e9367b51909fb551e5de8a09740064e842
MalBourbie <MalBourbie@gmail.com>
2025-01-27 18:38:58 +0100

    localization section on header

--- src/layout/Header/index.jsx
+++ src/layout/Header/index.jsx
@@ -1,27 +1,84 @@
 import m from "./index.module.scss"
 import githubIcon from '../../assets/icons/github.svg'
+import { useEffect, useState } from "react"
+
+const sections = [
+    { id: "about" },
+    { id: "skill" },
+    { id: "project" }
+]
+
+const headerList = [
+    {
+        link: "#about",
+        content: "A propos"
+    },
+    {
+        link: "#skill",
+        content: "Compétences"
+    },
+    {
+        link: "#project",
+        content: "Projets"
+    },
+    {
+        link: "#contact",
+        content: "Me Contacter"
+    },
+    {
+        link: "../../assets/cv/",
+        target: "_blank",
+        content: "CV"
+    }
+]
 
 function Header() {
+    const [activeSection, setActiveSection] = useState(null)
+
+    // to get the current section on the screen
+    useEffect(() => {
+        // get the sections in the DOM
+        const sectionElements = sections.map((section) =>
+            document.getElementById(section.id)
+        )
+
+        // set the active section in the state if isIntersecting
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    setActiveSection(entry.target.id)
+                }
+            })
+        }, { threshold: 0.65 })
+
+        // set the observer on each sections
+        sectionElements.forEach((element) => {
+            if (element) {
+                observer.observe(element)
+            }
+        })
+    }, [])
+
+    console.log(activeSection)
+
     return (
         <header className={m.header}>
             <h1>LPHoekstra</h1>
             <nav className={m.navBar}>
                 <ul>
-                    <li>
-                        <a href="#about" className={m.navBar__link}>A propos</a>
-                    </li>
-                    <li>
-                        <a href="#skill" className={m.navBar__link}>Compétences</a>
-                    </li>
-                    <li>
-                        <a href="#project" className={m.navBar__link}>Projets</a>
-                    </li>
-                    <li>
-                        <a href="#contact" className={m.navBar__link}>Me Contacter</a>
-                    </li>
-                    <li>
-                        <a href="../../assets/cv/" target="_blank" className={m.navBar__link}>CV</a>
-                    </li>
+                    {headerList.map((element) => (
+                        <li key={element.content}>
+                            <a href={element.link}
+                                target={element.target}
+                                className={`
+                                    ${m.navBar__link} 
+                                    ${element.link.split("#")[1] === activeSection ? m.navBar__link_selected : ""}
+                                `}
+                            >
+                                {element.content}
+                            </a>
+                        </li>
+                    ))}
                 </ul>
             </nav>
             <div>
